bd78baadcfa86f0d2523f79e9104825ac4bc29f8
NuryevArtem <Tyuiu.NuryevAR@outlook.com>
2024-09-19 23:24:55 +0500

    Task1

---
+++ Tyuiu.NuryevAR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.NuryevAR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Артём";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут "+name+" мне "+age+" лет!");
+
+            Console.ReadKey();
+        }
+    }
+}
