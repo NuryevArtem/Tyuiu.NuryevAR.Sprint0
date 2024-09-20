c0d02985a922400845c27e60a1e1f0b3e22d712e
NuryevArtem <Tyuiu.NuryevAR@outlook.com>
2024-09-20 20:01:48 +0500

    Task3

---
+++ Tyuiu.NuryevAR.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.NuryevAR.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.NuryevAR.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
